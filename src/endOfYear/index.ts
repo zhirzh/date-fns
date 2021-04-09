6ef53a2c4d2d202244e49be625a22d497ed278db
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2021-04-10 00:23:18 +0530

    endOfYear

--- src/endOfYear/index.ts
+++ src/endOfYear/index.ts
@@ -1,5 +1,7 @@
 import toDate from '../toDate/index'
+import { YearStartOptions } from '../types'
 import requiredArgs from '../_lib/requiredArgs/index'
+import toInteger from '../_lib/toInteger'
 
 /**
  * @name endOfYear
@@ -15,6 +17,8 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param {Date|Number} date - the original date
+ * @param {Object} [options] - an object with options.
+ * @param {0|1|2|3|4|5|6|7|8|9|10|11} [options.yearStartsOn=0] - the index of the first month of the year (0 - January)
  * @returns {Date} the end of a year
  * @throws {TypeError} 1 argument required
  *
@@ -22,13 +26,33 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * // The end of a year for 2 September 2014 11:55:00:
  * var result = endOfYear(new Date(2014, 8, 2, 11, 55, 00))
  * //=> Wed Dec 31 2014 23:59:59.999
+ *
+ * // If the year starts on April, the end of a year for 2 September 2014 11:55:00:
+ * const result = endOfYear(new Date(2014, 8, 2, 11, 55, 00), { yearStartsOn: 4 })
+ * //=> Tue Mar 31 2015 23:59:59.999
  */
-export default function endOfYear(dirtyDate: Date | number): Date {
+export default function endOfYear(
+  dirtyDate: Date | number,
+  dirtyOptions?: YearStartOptions
+): Date {
   requiredArgs(1, arguments)
 
-  var date = toDate(dirtyDate)
-  var year = date.getFullYear()
-  date.setFullYear(year + 1, 0, 0)
-  date.setHours(23, 59, 59, 999)
+  const options = dirtyOptions || {}
+  const yearStartsOn =
+    options.yearStartsOn == null ? 0 : toInteger(options.yearStartsOn)
+
+  // Test if yearStartsOn is between 0 and 11 _and_ is not NaN
+  if (!(yearStartsOn >= 0 && yearStartsOn <= 11)) {
+    throw new RangeError('yearStartsOn must be between 0 and 11 inclusively')
+  }
+
+  const cleanDate = toDate(dirtyDate)
+  const year = cleanDate.getFullYear()
+  const month = cleanDate.getMonth()
+
+  const date = new Date(0)
+  date.setFullYear(yearStartsOn > month ? year : year + 1, yearStartsOn)
+  date.setHours(0, 0, 0, 0)
+  date.setTime(date.getTime() - 1)
   return date
 }
--- src/endOfYear/test.ts
+++ src/endOfYear/test.ts
@@ -11,6 +11,25 @@ describe('endOfYear', function() {
     assert.deepEqual(result, new Date(2014, 11 /* Dec */, 31, 23, 59, 59, 999))
   })
 
+  it('allows to specify which day is the first day of the year', function() {
+    var date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
+    var result = endOfYear(date, { yearStartsOn: 1 })
+    assert.deepStrictEqual(
+      result,
+      new Date(2015, 0 /* Jan */, 31, 23, 59, 59, 999)
+    )
+  })
+
+  it('implicitly converts options', function() {
+    var date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
+    // @ts-expect-error
+    var result = endOfYear(date, { yearStartsOn: '1' })
+    assert.deepStrictEqual(
+      result,
+      new Date(2015, 0 /* Jan */, 31, 23, 59, 59, 999)
+    )
+  })
+
   it('accepts a timestamp', function() {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0).getTime()
     const result = endOfYear(date)
@@ -23,10 +42,56 @@ describe('endOfYear', function() {
     assert.deepEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0))
   })
 
+  describe('edge cases', function() {
+    describe('when the given day is before the start of a year', function() {
+      it('it returns the start of a year', function() {
+        const date = new Date(2014, 6 /* Jul */, 1)
+        const result = endOfYear(date, { yearStartsOn: 8 })
+        assert.deepEqual(
+          result,
+          new Date(2014, 7 /* Aug */, 31, 23, 59, 59, 999)
+        )
+      })
+    })
+
+    describe('when the given day is the start of a year', function() {
+      it('it returns the start of a year', function() {
+        const date = new Date(2014, 8 /* Sep */, 1)
+        const result = endOfYear(date, { yearStartsOn: 8 })
+        assert.deepEqual(
+          result,
+          new Date(2015, 7 /* Aug */, 31, 23, 59, 59, 999)
+        )
+      })
+    })
+
+    describe('when the given day is after the start of a year', function() {
+      it('it returns the start of a year', function() {
+        const date = new Date(2014, 10 /* Nov */, 1)
+        const result = endOfYear(date, { yearStartsOn: 8 })
+        assert.deepEqual(
+          result,
+          new Date(2015, 7 /* Aug */, 31, 23, 59, 59, 999)
+        )
+      })
+    })
+  })
+
   it('returns `Invalid Date` if the given date is invalid', function() {
     const result = endOfYear(new Date(NaN))
     //@ts-expect-error
     assert(result instanceof Date && isNaN(result))
+  })
+
+  it('throws `RangeError` if `options.yearStartsOn` is not convertable to 0, 1, ..., 11 or undefined', function() {
+    // @ts-expect-error
+    var block = endOfYear.bind(
+      null,
+      new Date(2014, 8 /* Sep */, 2, 11, 55, 0),
+      // $ExpectedMistake
+      { yearStartsOn: NaN }
+    )
+    assert.throws(block, RangeError)
   })
 
   it('throws TypeError exception if passed less than 1 argument', function() {
